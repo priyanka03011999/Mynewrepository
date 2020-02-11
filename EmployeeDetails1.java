27a5ba7c52275f51bd7c84ce17ecf80102a891e7
priyanka03011999 <60879045+priyanka03011999@users.noreply.github.com>
2020-02-11 15:45:27 +0530

    Add files via upload

---
+++ EmployeeDetails1.java
@@ -0,0 +1,23 @@
+public class EmployeeDetails1
+{
+int empId;
+String empName;
+float empSal;
+public EmployeeDetails1(){
+}
+public EmployeeDetails1(int empId,String empName,float empSal)
+{
+this.empId=empId;
+this.empName=empName;
+this.empSal=empSal;
+}
+public void calculateSalary()
+{
+System.out.println("employee salary:"+(empsal+2000));
+}
+public static void main(String[] args)
+{
+Employeedetails1 ed= new Employeedetails1();
+ed.calculateSalary();
+}
+)
