db564ee1609d8c15222d592e0d72a35e922cd02f
priyanka03011999 <60879045+priyanka03011999@users.noreply.github.com>
2020-02-11 16:01:43 +0530

    Update EmployeeDetails1.java

--- EmployeeDetails1.java
+++ EmployeeDetails1.java
@@ -3,18 +3,20 @@ public class EmployeeDetails1
 int empId;
 String empName;
 float empSal;
-public EmployeeDetails1(){
-}
+  
+public EmployeeDetails1(){}
 public EmployeeDetails1(int empId,String empName,float empSal)
 {
 this.empId=empId;
 this.empName=empName;
 this.empSal=empSal;
 }
+  
 public void calculateSalary()
 {
 System.out.println("employee salary:"+(empSal+2000));
 }
+  
 public static void main(String[] args)
 {
 EmployeeDetails1 ed= new EmployeeDetails1();
